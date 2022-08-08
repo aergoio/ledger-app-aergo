666fa27be08281afcef0fe81669b564c04f4eb70
Bernardo Ramos <bernardo@blocko.io>
2022-08-08 02:26:51 -0300

    fix test case

--- tests/speculos/test_error_cmd.py
+++ tests/speculos/test_error_cmd.py
@@ -23,20 +23,18 @@ def test_bad_ins(client):
         raise DeviceException(error_code=error.sw)
 
 
-@pytest.mark.xfail(raises=WrongP1P2Error)
-def test_wrong_p1p2(client):
-    try:
-        client.apdu_exchange(cla=0xAE,
-                             ins=0x03,
-                             p1=0x01)  # 0x01 instead of 0x00
-    except ApduException as error:
-        raise DeviceException(error_code=error.sw)
-
-
 @pytest.mark.xfail(raises=WrongDataLengthError)
 def test_wrong_data_length(client):
     try:
         # APDUs must be at least 5 bytes: CLA, INS, P1, P2, Lc.
         client._apdu_exchange(b"AE00")
+    except ApduException as error:
+        raise DeviceException(error_code=error.sw)
+
+
+@pytest.mark.xfail(raises=WrongDataLengthError)
+def test_wrong_lc(client):
+    try:
+        client._apdu_exchange(b"AE0003000011")
     except ApduException as error:
         raise DeviceException(error_code=error.sw)
